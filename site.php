a6575a9a936e99de48db656a2f38af9af81c0332
queenrats <81180378+queenrats@users.noreply.github.com>
2021-03-30 17:01:17 +0100

    Add files via upload

--- site.php
+++ site.php
@@ -1,11 +1,14 @@
 <html>
     <head>
-            <title>PHP Test</title>
+            <title>Debug Panel</title>
     </head>
 
 
     <body>
-        <?php echo '<p>Hello World!</p>'; ?>
+        <h1>Debug Panel</h1>
+        <h2>FOR INTERNAL USE ONLY</h2>
+        <p>This panel lets you see DB info for each user. Admin details are not accessible for security reasons.</p>
+        <br><br><br><br>
         <form name="form" action="" method="post">
         <label for="id_field">Please enter User ID:</label>
         <input type="text" id="id_field" name="id_field">
@@ -30,10 +33,12 @@
      $sql = "SELECT * FROM Users WHERE UserID = ". $search;
     $result = mysqli_query($conn, $sql);
 
-    if (mysqli_num_rows($result) > 0) {
-
+    if (!empty($result)) {
+        echo "Selected User ID details:<br><br>";
         while($row = mysqli_fetch_assoc($result)) {
-            echo "ID: " . $row["UserID"]. " - Username: ". $row["Username"]. " - Password: ". $row["Password"]. "<br>";
+            if ($row["isAdmin"] = 1){
+                echo "ID: " . $row["UserID"]. " - Username: ". $row["Username"]. " - Password: ". $row["Password"]. "<br>";
+            }
         }
         } else {
             echo "0 results";
@@ -42,5 +47,7 @@
         
     mysqli_close($conn);    
         ?>
+    <br><br><br><br><br>
+    <a href="admin.php">Admin Panel</a>
     </body>
 </html>
