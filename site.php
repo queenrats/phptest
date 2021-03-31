26f4f0bfacfe97c40d8e99b25e1404b5295e6f5e
queenrats <81180378+queenrats@users.noreply.github.com>
2021-03-31 20:30:17 +0100

    Add files via upload

--- site.php
+++ site.php
@@ -6,11 +6,11 @@
 
     <body>
         <h1>Debug Panel</h1>
-        <h2>FOR INTERNAL USE ONLY</h2>
-        <p>This panel lets you see DB info for each user. Admin details are not accessible for security reasons.</p>
+        <h2>FOR INTERNAL ADMIN USE ONLY</h2>
+        <p>This panel lets you see DB info for each user without having to connect directly to the MySQL database, as long as you know their username.</p>
         <br><br><br><br>
         <form name="form" action="" method="post">
-        <label for="id_field">Please enter User ID:</label>
+        <label for="id_field">Please enter a Username:</label>
         <input type="text" id="id_field" name="id_field">
     </form>
 
@@ -25,20 +25,19 @@
     if (!$conn){
         die("Connection failed: " . mysqli_connect_error());
     }
-    echo "Connected successfully";
+    echo "Connected to MySQL server successfully<br><br>";
     
     
     $search = $_POST['id_field'];
 
-     $sql = "SELECT * FROM Users WHERE UserID = ". $search;
+    $sql = "SELECT * FROM Users WHERE Username = '". $search. "'";
     $result = mysqli_query($conn, $sql);
 
     if (!empty($result)) {
-        echo "Selected User ID details:<br><br>";
+        echo "Selected Username details:<br><br>";
         while($row = mysqli_fetch_assoc($result)) {
-            if ($row["isAdmin"] = 1){
-                echo "ID: " . $row["UserID"]. " - Username: ". $row["Username"]. " - Password: ". $row["Password"]. "<br>";
-            }
+            echo "ID: " . $row["UserID"]. " - Username: ". $row["Username"]. " - Password: ". $row["Password"]. " - isAdmin: ". $row["isAdmin"]. "<br>";
+            
         }
         } else {
             echo "0 results";
